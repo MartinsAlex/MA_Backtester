457523ee94a15a3a735564d2bf60ec754f75df45
MartinsAlex <63752184+MartinsAlex@users.noreply.github.com>
2020-04-22 01:25:53 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+   name='foo',
+   version='1.0',
+   description='A useful module',
+   author='Man Foo',
+   author_email='foomail@foo.com',
+   packages=['MA_Backtester'],  #same as name
+   install_requires=['matplotlib', 'pandas', "numpy", "pandas-datareader"], #external packages as dependencies
+   scripts=[
+            'MA_Backtester.py'
+           ]
+)
