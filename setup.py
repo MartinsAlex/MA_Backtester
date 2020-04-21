7c016d7c39b52ac84610686c2d06eda915e4ba5c
MartinsAlex <63752184+MartinsAlex@users.noreply.github.com>
2020-04-22 01:48:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='backesting',
-    version='1.0',
-    packages=['MA_Backtester'],
-    url='',
-    license='mit',
-    author='Alex',
-    author_email='',
-    description=''
+
+setuptools.setup(
+    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
