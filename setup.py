73de16b5859c7974408a1e794ab3b9899ee097f7
MartinsAlex <63752184+MartinsAlex@users.noreply.github.com>
2020-04-22 13:47:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setuptools.setup(
     name="MA_Backtester", # Replace with your own username
     version="0.0.1",
     author="Example Author",
-    author_email="author@example.com",
+    author_email="alexandre.martins-figueira@etu.hesge.ch",
     description="A small example package",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/MartinsAlex/MA_Backtester",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
