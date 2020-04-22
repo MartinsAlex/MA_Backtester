19f05e66557900d27b4e92b146acbfd9ccace38c
MartinsAlex <63752184+MartinsAlex@users.noreply.github.com>
2020-04-22 02:25:33 +0200

    Delete moduleTester.py

--- moduleTester.py
+++
@@ -1,9 +0,0 @@
-import MA_Backtester as mab
-
-strat = mab.movingAverageCrossover(["AAPL", "TSLA"], 50, 200, "2017-01-01", "2020-01-01", shortLong="both", capital=1_000_000, maType="exp", makeplot=True)
-
-strat.analyse()
-
-print(strat.resultsTable)
-
-print(strat.transactionTable)
