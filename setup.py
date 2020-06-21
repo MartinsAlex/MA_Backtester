8bf1407770bdffbe88a452559861cc52c994dcf5
MartinsAlex <63752184+MartinsAlex@users.noreply.github.com>
2020-06-21 20:06:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 import setuptools
 
+import os
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = [] # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
 
 setuptools.setup(
     name="MA_Backtester",
@@ -13,4 +22,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
     ],
     python_requires='>=3.6',
+    install_requires=install_requires
 )
