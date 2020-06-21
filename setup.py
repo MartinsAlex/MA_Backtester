7240939507762f65856c63c99c63f1136bb507c2
MartinsAlex <63752184+MartinsAlex@users.noreply.github.com>
2020-06-21 19:30:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     author="Alexandre MARTINS",
     author_email="alexandre.martins-figueira@etu.hesge.ch",
     url="https://github.com/MartinsAlex/MA_Backtester",
-    packages=setuptools.find_packages(),
+    packages=['MA_Backtester', 'MA_Backtester'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
